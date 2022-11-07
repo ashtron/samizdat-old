2871fb000b3c3e6d8e135aae47e14090bee34abf
ashtron <ashton.playsted@protonmail.com>
2022-11-06 16:54:09 -0800

    Handle tab change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
+import './styles/main.css';
 import MainPanel from "./MainPanel";
 import BookForm from "./BookForm";
 
--- src/MainPanel.tsx
+++ src/MainPanel.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.min.css';
 import BookForm from './BookForm'
 
 function MainPanel({ children }: { children: React.ReactNode }) {
-  console.log(children)
+  const [selectedTab, setSelectedTab] = useState("book");
+
+  function handleTabClick(event: React.MouseEvent<Element, MouseEvent>) {
+    if (!event.currentTarget.classList.contains("is-active"))
+      toggleActiveTab();
+  }
+
+  function toggleActiveTab() {
+    document.getElementById("books-tab")?.classList.toggle("is-active");
+    document.getElementById("movies-tab")?.classList.toggle("is-active");
+  }
+
   return (
-    <section className="container">
+    <section className="container" style={{ marginTop: "10%", width: "60%" }}>
       <div className="panel" style={{ padding: "0px 24px 24px 24px" }}>
-        <div className="tabs is-centered is-medium">
+        <div className="tabs is-centered is-medium is-boxed">
           <ul>
 
-            <li className="is-active">
+            <li id="books-tab" className={ selectedTab === "book" ? "is-active" : "" } onClick={ handleTabClick }>
               <a>
                 <span>Books</span>
               </a>
             </li>
     
-            <li>
+            <li id="movies-tab" className={ selectedTab === "movie" ? "is-active" : "" } onClick={ handleTabClick }>
               <a>
                 <span>Movies</span>
               </a>
